14569c3f46516d3b748d2c1c843f644f02d24a06
codename-white <pcrosz310@gmail.com>
2025-11-22 15:21:02 +0700

    Initial commit

---
+++ app/flexbox/ex09.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function Ex05() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'space-around',  
+        alignItems: 'stretch',
+      }}
+    >
+
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between', 
+        }}
+      >
+        <View style={{ width: 100, height: 100, backgroundColor: '#50E3C2' }} />
+        <View style={{ width: 100, height: 100, backgroundColor: '#50E3C2' }} />
+        <View style={{ width: 100, height: 100, backgroundColor: '#50E3C2' }} />
+      </View>
+
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}
+      >
+        <View style={{ width: 100, height: 100, backgroundColor: '#4A90E2' }} />
+        <View style={{ width: 100, height: 100, backgroundColor: '#4A90E2' }} />
+        <View style={{ width: 100, height: 100, backgroundColor: '#4A90E2' }} />
+      </View>
+
+      {/* Row 3 */}
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}
+      >
+        <View style={{ width: 100, height: 100, backgroundColor: '#9013FE' }} />
+        <View style={{ width: 100, height: 100, backgroundColor: '#9013FE' }} />
+        <View style={{ width: 100, height: 100, backgroundColor: '#9013FE' }} />
+      </View>
+    </View>
+  );
+}
